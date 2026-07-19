cedd6c7abd7ceb3f3a05bf4fa28f4bb86c07bb49
Arjav Jain <arjavjain0703@gmail.com>
2026-07-19 16:45:18 +0530

    create a book!

--- script.js
+++ script.js
@@ -12,17 +12,11 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff01 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-
-const donut_geo = new THREE.TorusGeometry(10, 3, 16, 100);
-const donue_texture = new THREE.TextureLoader().load('https://upload.wikimedia.org/wikipedia/commons/a/ab/Donut_texture%2C_Doughnut.jpg');
-const donut_tex = new THREE.MeshBasicMaterial({ map: donue_texture });
-const donut = new THREE.Mesh(donut_geo, donut_tex);
-scene.add(donut);
+// const donut_geo = new THREE.TorusGeometry(10, 3, 16, 100);
+// const donue_texture = new THREE.TextureLoader().load('https://upload.wikimedia.org/wikipedia/commons/a/ab/Donut_texture%2C_Doughnut.jpg');
+// const donut_tex = new THREE.MeshBasicMaterial({ map: donue_texture });
+// const donut = new THREE.Mesh(donut_geo, donut_tex);
+// scene.add(donut);
 
 
 camera.position.z = 45;
@@ -31,11 +25,14 @@ function animate() {
 
   requestAnimationFrame(animate);
 
-  donut.rotation.x += 0.01;
-  donut.rotation.y += 0.01;
+  // donut.rotation.x += 0.01;
+  // donut.rotation.y += 0.01;
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  const book = scene.getObjectByName('book');
+  // book.rotation.x += 0.01;
+  book.rotation.y += 0.02;
+
+
 
   renderer.render( scene, camera );
 }
@@ -43,17 +40,14 @@ function animate() {
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
 
-  cube.rotation.y += 0.01;
-  cube.rotation.z += 0.01;
-
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0000;
   camera.rotation.y = t * -0.0000;
 
 }
 
-document.body.onscroll = moveCamera;
-moveCamera();
+// document.body.onscroll = moveCamera;
+// moveCamera();
 
 
 
@@ -71,6 +65,19 @@ function add_star() {
 }
 
 Array(200).fill().forEach(add_star);
+
+
+function addbook(coverURL) {
+  const book_geometry = new THREE.BoxGeometry(10, 15, 1);
+  const book_material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load(coverURL) });
+  const book = new THREE.Mesh(book_geometry, book_material);
+  book.rotation.z += 0.2;
+  scene.add(book);
+  return book;
+}
+
+const book = addbook("https://m.media-amazon.com/images/I/81msb6gUBTL._AC_UF1000,1000_QL80_.jpg");
+book.name = 'book';
 
 
 animate();
