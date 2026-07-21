39dd7336bfddb04486f2b11f3732e71cb52aaaa9
Arjav Jain <arjavjain0703@gmail.com>
2026-07-21 13:43:17 +0530

    add song yayaya

--- script.js
+++ script.js
@@ -141,3 +141,16 @@ window.addEventListener('resize', () => {
 });
 
 animate();
+
+
+const listener = new THREE.AudioListener();
+camera.add( listener );
+const sound = new THREE.Audio( listener );
+
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load( 'song.webm', function( buffer ) {
+	sound.setBuffer( buffer );
+	sound.setLoop( true );
+	sound.setVolume( 0.5 );
+	sound.play();
+});
